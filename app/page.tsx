77137122a19722b5e50d16fa4139b35257ec3a6a
sakuraku-admin <aburisakeharasu@gmail.com>
2026-03-25 23:59:58 +0900

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <main
+      style={{
+        height: "100vh",
+        backgroundImage: "url('/images/top-visual.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    />
+  )
+}
