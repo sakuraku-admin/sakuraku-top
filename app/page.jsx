b56cbefe2005c777798f02955c6bba443fdbfbb7
sakuraku-admin <aburisakeharasu@gmail.com>
2026-03-27 01:28:09 +0900

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,99 +3,106 @@ export default function Home() {
     <main
       style={{
         minHeight: "100vh",
-        backgroundImage: "url('/images/top-visual.png')",
-        backgroundSize: "cover",
-        backgroundPosition: "center top",
-        backgroundRepeat: "no-repeat",
-        position: "relative",
+        margin: 0,
+        backgroundColor: "#f6c39d",
+        display: "flex",
+        justifyContent: "center",
       }}
     >
       <div
         style={{
+          width: "100%",
+          maxWidth: "900px",
           minHeight: "100vh",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "flex-start",
-          paddingTop: "58vh",
-          boxSizing: "border-box",
+          backgroundImage: "url('/images/top-visual.png')",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center top",
+          backgroundSize: "100% auto",
+          position: "relative",
         }}
       >
-        <a
-          href="/reserve"
-          style={{
-            display: "inline-flex",
-            alignItems: "center",
-            justifyContent: "center",
-            width: "320px",
-            maxWidth: "80vw",
-            height: "78px",
-            borderRadius: "999px",
-            backgroundColor: "#8b5a3c",
-            color: "#f7d9d2",
-            textDecoration: "none",
-            fontSize: "30px",
-            fontWeight: "bold",
-            letterSpacing: "0.05em",
-            border: "4px solid #1f120d",
-            boxShadow: "0 2px 0 rgba(0,0,0,0.15)",
-            textShadow:
-              "-1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff, 1px 1px 0 #fff",
-          }}
-        >
-          予約する
-        </a>
-
         <div
           style={{
-            marginTop: "42px",
+            paddingTop: "62%",
             display: "flex",
-            gap: "44px",
-            flexWrap: "wrap",
-            justifyContent: "center",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
           <a
-            href="/confirm"
+            href="/reserve"
             style={{
               display: "inline-flex",
               alignItems: "center",
               justifyContent: "center",
-              width: "190px",
-              maxWidth: "38vw",
-              height: "64px",
+              width: "260px",
+              height: "74px",
               borderRadius: "999px",
-              backgroundColor: "#8b5a3c",
-              color: "#d8b39b",
+              backgroundColor: "#8f5d40",
+              color: "#f6d7d0",
               textDecoration: "none",
-              fontSize: "22px",
+              fontSize: "28px",
               fontWeight: "bold",
               border: "4px solid #1f120d",
+              boxSizing: "border-box",
+              textShadow:
+                "-1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff, 1px 1px 0 #fff",
             }}
           >
-            予約確認
+            予約する
           </a>
 
-          <a
-            href="/info"
+          <div
             style={{
-              display: "inline-flex",
-              alignItems: "center",
+              marginTop: "34px",
+              display: "flex",
+              gap: "36px",
               justifyContent: "center",
-              width: "190px",
-              maxWidth: "38vw",
-              height: "64px",
-              borderRadius: "999px",
-              backgroundColor: "#8b5a3c",
-              color: "#d8b39b",
-              textDecoration: "none",
-              fontSize: "22px",
-              fontWeight: "bold",
-              border: "4px solid #1f120d",
+              flexWrap: "wrap",
             }}
           >
-            店舗情報
-          </a>
+            <a
+              href="/confirm"
+              style={{
+                display: "inline-flex",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "170px",
+                height: "58px",
+                borderRadius: "999px",
+                backgroundColor: "#8f5d40",
+                color: "#d9b29a",
+                textDecoration: "none",
+                fontSize: "18px",
+                fontWeight: "bold",
+                border: "4px solid #1f120d",
+                boxSizing: "border-box",
+              }}
+            >
+              予約確認
+            </a>
+
+            <a
+              href="/info"
+              style={{
+                display: "inline-flex",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "170px",
+                height: "58px",
+                borderRadius: "999px",
+                backgroundColor: "#8f5d40",
+                color: "#d9b29a",
+                textDecoration: "none",
+                fontSize: "18px",
+                fontWeight: "bold",
+                border: "4px solid #1f120d",
+                boxSizing: "border-box",
+              }}
+            >
+              店舗情報
+            </a>
+          </div>
         </div>
       </div>
     </main>
