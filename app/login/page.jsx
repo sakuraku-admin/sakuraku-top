5fccccc43a055c36ace340efb2293d9bdf94396a
sakuraku-admin <aburisakeharasu@gmail.com>
2026-03-29 01:58:21 +0900

    Update page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -67,7 +67,7 @@ export default function LoginPage() {
   const subButtonStyle = {
     position: "absolute",
     left: "73%",
-    top: "31%",
+    top: "35%",
     transform: "translateX(-50%)",
     display: "flex",
     alignItems: "center",
