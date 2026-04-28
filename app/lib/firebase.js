b9e7133a3d7bbce6745ac127e7bd4c7f7382e18b
sakuraku-admin <aburisakeharasu@gmail.com>
2026-04-28 20:33:02 +0900

    Create firebase.js

---
+++ app/lib/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAIS9CcjV2kNXlUDqcL_UXIK-lUf6764hY",
+  authDomain: "sakuraku-60be5.firebaseapp.com",
+  projectId: "sakuraku-60be5",
+  storageBucket: "sakuraku-60be5.firebasestorage.app",
+  messagingSenderId: "800995227717",
+  appId: "1:800995227717:web:8101e3431986152d1d4b2c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
