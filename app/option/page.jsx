595c025f790d43945641dfa4be9c39d81f70a79d
sakuraku-admin <aburisakeharasu@gmail.com>
2026-08-13 04:15:12 +0900

    Update page.jsx

--- app/option/page.jsx
+++ app/option/page.jsx
@@ -180,7 +180,7 @@ function OptionMenuContent() {
 
          <section style={styles.block}>
             <h2 style={styles.sectionTitle}>・炭酸ヘッドスパ
-              <span style={styles.subTitle}>（＼期間限定／）</span>
+              <span style={styles.subTitle, color: "#e68a7a",}>（＼期間限定／）</span>
             </h2>
             <div style={styles.priceLine}>500円</div>
             <p style={styles.description}>
